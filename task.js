7cc339dff4b0639777bec4d7f3d900c0dd893a34
Maxwell <maxwellhilmo@gmail.com>
2025-03-19 21:02:02 -0600

    Wed Mar 19 21:02:02 MDT 2025

--- task.js
+++ task.js
@@ -1,11 +1,7 @@
-let text = document.getElementById("new-task");
-    
-console.log(text);
-/*
 const lists = {
     1: {
         name: "School Tasks",
-        todos: [
+        tasks: [
             {
                 text: 'Math',
                 completed: false
@@ -22,7 +18,7 @@ const lists = {
     },
     2: {
         name: "Shopping List",
-        todos: [
+        tasks: [
             {
                 text: 'Apples',
                 completed: false
@@ -39,14 +35,14 @@ const lists = {
     }
 }
 
-const currentList = lists[0];
+const currentList = lists[1];
 
 function render() {
     // this will hold the html that will be displayed in the sidebar
     let listsHtml = '<ul class="list-group">';
     // iterate through the lists to get their names
     lists.forEach((list) => {
-      listsHtml += `<li class="list-group-item">${list.name}</li>`;
+      listsHtml += `<li class="pl-2 pr-2 ml-4 mr-4 mb-2 mt-2 bg-gray-300">${list.name}</li>`;
     });
    
     listsHtml += '</ul>';
@@ -58,27 +54,51 @@ function render() {
     document.getElementById('current-list-name').innerText = currentList.name;
     // iterate over the todos in the current list
    
-    let todosHtml = '<ul class="list-group-flush">';
-    currentList.todos.forEach((list) => {
-      todosHtml += `<li class="list-group-item">${todo.text}</li>`;
+    let tasksHtml = '<div class="ml-4 mr-4 mt-2 outline outline-black flex justify-between task-body"';
+    currentList.tasks.forEach((list) => {
+      tasksHtml += `<label class="list-group-item">${task.text}</label>`;
     });
     // print out the todos
-    document.getElementById('current-list-todos').innerHTML = todosHtml;
+    document.getElementById('current-list-tasks').innerHTML = tasksHtml;
    }
 
-   function addTodo() {
-    // get the todo text from the todo input box
-    const text = document.getElementById('todo-input-box').value;
+   function addList () {
+    //get the list text from the list input box
+    const text = document.getElementById('new-task').value;
     if(text) {
-      currentList.todos.push({
-        text: text,
-        completed: false
+      currentList.tasks.push({
+        text: text
       })
       render();
     }
    }
 
+   function removeList () {}
+
+   function addTasks() {
+    const text = document.getElementById('new-task').value;
+    if(text) {
+      currentList.tasks.push({
+        text: text,
+        completed: false
+      })
+      console.log(currentList.task);
+      render();
+    }
+    console.log(`nothin' added`);
+   }//add function for enter key
+
+   function checkbox(id){
+    let box = documen.getElementById()
+   }
+
+   function removeTasks () {}
+
+   function removeAllCompletedTasks () {}
+
+   function deleteList () {}
+
    function save() {
     localStorage.setItem('currentList', JSON.stringify(currentList)); 
     localStorage.setItem('lists', JSON.stringify(lists));
-   } */
+   }
